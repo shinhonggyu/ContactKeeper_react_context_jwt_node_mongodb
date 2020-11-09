3d5723de60719b5e6a57c6551d4b0306dd97e290
honggyu <zowoz0@naver.com>
2020-11-09 22:33:34 +0900

    Hash Passwords & Register

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { body, validationResult, check } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const { validationResult, check } = require('express-validator');
 
 const User = require('../models/User');
 
@@ -17,13 +18,40 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed');
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      // before save it to the DB we need to encrypt the password or hash the password with bcrypt
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // user 받기전에 jwt 생성
+      res.send('User saved');
+    } catch (error) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 

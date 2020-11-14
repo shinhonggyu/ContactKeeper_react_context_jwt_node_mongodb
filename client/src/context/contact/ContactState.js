0521aafb1e424216c79481312177166eb1b08610
honggyu <zowoz8819@gmail.com>
2020-11-14 14:42:51 +0900

    Delete Contact From UI

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -51,6 +51,12 @@ const ContactState = (props) => {
   };
 
   // Delete Contact
+  const deleteContact = (id) => {
+    dispatch({
+      type: DELETE_CONTACT,
+      payload: id,
+    });
+  };
 
   // Set Current Contact
 
@@ -67,6 +73,7 @@ const ContactState = (props) => {
       value={{
         contacts: state.contacts,
         addContact,
+        deleteContact,
       }}
     >
       {props.children}

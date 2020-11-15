112b9ceda13a9c48937b6b178d48103d4d31bb91
shinhonggyu <zowoz0@naver.com>
2020-11-15 22:00:30 +0900

    Alert Context, State & Component

--- client/src/App.js
+++ client/src/App.js
@@ -5,37 +5,41 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 import './App.css';
-import { LOGIN_FAIL } from './context/types';
 
 const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/">
-                  <Home />
-                </Route>
-                <Route exact path="/about">
-                  <About />
-                </Route>
-                <Route exact path="/register">
-                  <Register />
-                </Route>
-                <Route exact path="/login">
-                  <Login />
-                </Route>
-              </Switch>
-            </div>
-          </>
-        </Router>
+        <AlertState>
+          <Router>
+            <>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path="/">
+                    <Home />
+                  </Route>
+                  <Route exact path="/about">
+                    <About />
+                  </Route>
+                  <Route exact path="/register">
+                    <Register />
+                  </Route>
+                  <Route exact path="/login">
+                    <Login />
+                  </Route>
+                </Switch>
+              </div>
+            </>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
